e31a2bc4d51494cdebc2b6782727f2bab116963b
Overseven <specregister@gmail.com>
2021-02-11 23:38:03 +0300

    Add Node/Client modes

--- main.go
+++ main.go
@@ -1,79 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-	tr "github.com/overseven/blockchain/transaction"
-
-	cr "github.com/ethereum/go-ethereum/crypto"
+	node "github.com/overseven/blockchain/node"
+	client "github.com/overseven/blockchain/client"
 )
 
 func main() {
-	transaction := tr.Transaction{}
-	privkey, err := cr.GenerateKey()
-	if err != nil {
-		panic(err)
+	flagNode := flag.Bool("node", false, "start as node")
+	flagClient := flag.Bool("client", false, "start as client")
+
+	flag.Parse()
+
+	if *flagNode {
+		fmt.Println("Node choosen!")
+		node.Run()
+
+	}else if *flagClient {
+		fmt.Println("Client choosen!")
+		client.Run()
+
+	} else{
+		fmt.Println("Choose mode. Use \"-h\" param.")
+		return
 	}
-	privkey2, err := cr.GenerateKey()
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Println("Private key:", privkey.D)
-
-	pubkey := cr.CompressPubkey(&privkey.PublicKey)
-	pubkey2 := cr.CompressPubkey(&privkey2.PublicKey)
-	transaction.Pubkey = pubkey
-	transaction.Pay = 14
-	transaction.Fee = 0.123
-	transaction.Receiver = pubkey2
-	transaction.Message = "memsage"
-	{
-		t := time.Now()
-		transaction.Timestamp = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.UTC)
-	}
-	hashed := tr.GetHash(&transaction)
-
-	//fmt.Println("Public key:", pubkey)
-
-	sign, err := cr.Sign(hashed, privkey)
-	if err != nil {
-		panic(err)
-	}
-
-	transaction.Sign = sign
-	//spew.Dump(transaction)
-
-	jsonTr, err := json.Marshal(transaction)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(jsonTr))
-
-	reverseTr := tr.FromJSON(jsonTr)
-	valid := tr.Verify(&reverseTr)
-	fmt.Println("Valid:", valid)
-	url := "http://127.0.0.1:8090/test"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonTr))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+	return
 }
--- node/node.go
+++ node/node.go
@@ -1,3 +1,30 @@
 package node
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	tr "github.com/overseven/blockchain/transaction"
 
+	//"github.com/davecgh/go-spew/spew"
+)
+
+func Run(){
+	fmt.Println("Launching server...")
+	http.HandleFunc("/test", test)
+	http.ListenAndServe(":8090", nil)
+}
+
+func test(w http.ResponseWriter, req *http.Request) {
+	var t tr.Transaction
+	err := json.NewDecoder(req.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//spew.Dump(t)
+
+	valid := tr.Verify(&t)
+	fmt.Println("Receive transaction, valid: ", valid)
+}
