ecfbe9d59cb28d3f9312ce5244c98d25ef1ad4c3
Igor <specregister@gmail.com>
2021-03-23 15:19:17 +0300

    add PrivtoPubKey conversion function

--- utility/utility.go
+++ utility/utility.go
@@ -1,26 +1,33 @@
-package blUtility
+package utility
 
 import (
-    "encoding/binary"
-    "math"
+	"crypto/ecdsa"
+	"encoding/binary"
+	"math"
+
+	cr "github.com/ethereum/go-ethereum/crypto"
 )
 
-func Int64FromBytes(bytes []byte) uint64{
-    return uint64(binary.LittleEndian.Uint64(bytes))
+func Int64FromBytes(bytes []byte) uint64 {
+	return uint64(binary.LittleEndian.Uint64(bytes))
 }
 
 func UInt64Bytes(value uint64) []byte {
-    bytes := make([]byte, 8)
-    binary.LittleEndian.PutUint64(bytes, value)
-    return bytes
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, value)
+	return bytes
 }
 
 func Float64FromBytes(bytes []byte) float64 {
-    bits := binary.LittleEndian.Uint64(bytes)
-    float := math.Float64frombits(bits)
-    return float
+	bits := binary.LittleEndian.Uint64(bytes)
+	float := math.Float64frombits(bits)
+	return float
 }
 
 func Float64Bytes(float float64) []byte {
-    return UInt64Bytes(math.Float64bits(float))
+	return UInt64Bytes(math.Float64bits(float))
+}
+
+func PrivToPubKey(sndrPrivKey *ecdsa.PrivateKey) []byte {
+	return cr.CompressPubkey(&sndrPrivKey.PublicKey)
 }
