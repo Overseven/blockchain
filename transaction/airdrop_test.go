13d45820e299dd9dd8010ebfe9c71cc6aec002b8
Overseven <specregister@gmail.com>
2021-03-31 22:43:04 +0300

    init airdrop_test

--- transaction/airdrop_test.go
+++ transaction/airdrop_test.go
@@ -1,1 +1,87 @@
-package transaction
+package transaction_test
+
+import (
+	cr "github.com/ethereum/go-ethereum/crypto"
+	"github.com/overseven/blockchain/balance"
+	"github.com/overseven/blockchain/interfaces"
+	"github.com/overseven/blockchain/utility"
+	"github.com/overseven/blockchain/wallet"
+	"github.com/overseven/blockchain/transaction"
+	"testing"
+)
+
+const airdropModeratorConfigFile = "..\\wallet.cfg"
+
+func TestAirdrop_GetData(t *testing.T) {
+	data := interfaces.Data{}
+	pr1, _, err := utility.GenerateWallet()
+	if err != nil {
+		t.Error(err)
+	}
+	pr1Bytes := utility.PrivToPubKey(pr1)
+
+	_, pub2, err := utility.GenerateWallet()
+	if err != nil {
+		t.Error(err)
+	}
+	value := 65.32
+	fee := 42.2222
+	message := "test"
+	timestamp := utility.NewTimestamp()
+
+	data.Type = transaction.TypeTransfer
+	data.Sender = pr1Bytes
+	data.Receiver = pub2
+	data.Pay = value
+	data.Fee = fee
+	data.Message = message
+	data.Timestamp = timestamp
+	data.Sign = []byte("eeee sign!")
+
+	tr1 := transaction.Transfer{Data: data}
+
+	if !transaction.IsEqual(&data, tr1.GetData(), true) {
+		t.Error("err: data and tr1.data are not equal!")
+	}
+
+	data.Message = "Press F"
+
+	if transaction.IsEqual(&data, tr1.GetData(), true) {
+		t.Error("err: modified data and tr1.data are false equal!")
+	}
+}
+func TestNewAirdrop(t *testing.T) {
+
+}
+
+func TestAirdropVerify(t *testing.T) {
+	var usersBalance interfaces.Balancer = &balance.Balance{}
+	usersBalance.Init()
+
+	_, receiver1, err := utility.GenerateWallet()
+	if err != nil {
+		t.Error(err)
+	}
+
+	airdropPubKey, airdropPrKey, err := wallet.LoadFromFile(airdropModeratorConfigFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	transaction.AirDropModeratorPubKey = airdropPubKey
+
+	airPrKey, err := cr.ToECDSA(airdropPrKey[:32])
+	if err != nil {
+		t.Error(err)
+	}
+
+	airdrop, err := transaction. NewAirdrop(receiver1, airPrKey, 110.1, 11.1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := airdrop.Verify(usersBalance); err != nil {
+		t.Error(err)
+	}
+
+}
